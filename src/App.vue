264af501873afe7f963b0c2e544802656783532b
Michel <kingkong12666@gmail.com>
2018-10-23 12:57:22 +0300

    init page

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-app>
+    <header>
+      <img src="image/Logo_layer.png" alt="">
+      <span>+971 56 437 7612</span>
+    </header>
+  </v-app>
+</template>
+
+<script>
+
+export default {
+  data () {
+    return {
+
+    }
+  },
+  name: 'App',
+  components: {
+  }
+}
+</script>
+<style>
+  header{
+    background-color: #002258;
+    width: 100%;
+    height: 40px;
+  }
+  span{
+    color: white;
+    font-size: 14px;
+  }
+</style>
